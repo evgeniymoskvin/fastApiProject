1e607331eebc5f792b63c680fcac0a5e6429707f
E.I.Moskvin <evgeni.moskvin@gmail.com>
2024-09-10 09:49:51 +0300

    upd

--- methods.py
+++ methods.py
@@ -50,7 +50,7 @@ def delete_files_from_bucket(files_dict: dict, client=cl) -> dict:
     }
     for key, file_name in files_dict['files'].items():
         if client.bucket_exists(files_dict['bucket_name']):  # проверяем существование корзины
-            try:  # Отлавливаем ошибку, если файл был удален из корзины, но остался в бд
+            try:  # Если файл был удален из корзины, но остался в бд
                 client.get_object(files_dict['bucket_name'], file_name)
                 client.remove_object(files_dict['bucket_name'], file_name)
                 result_dict[next(number_file)] = file_name
